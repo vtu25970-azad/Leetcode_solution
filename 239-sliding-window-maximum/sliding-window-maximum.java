ef6c034bad52385a6078036b78b772c6e426ad7f
vtu25970-azad <vtu25970@veltech.edu.in>
2026-04-08 11:04:35 +0530

    Time: 32 ms (41.04%) | Memory: 146.9 MB (47.75%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                result[idx++] = nums[deque.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
