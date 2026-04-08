8d09dc955953135225df0a3542d103e97be64f10
vtu25970-azad <vtu25970@veltech.edu.in>
2026-04-08 11:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (59.77%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isMirror(root.left,root.right);
+    }
+    private boolean isMirror(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        if(left==null || right==null){
+            return false;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return isMirror(left.left,right.right) && isMirror(left.right,right.left);
+    }
+}
